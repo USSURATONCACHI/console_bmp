712c75830b2128e9b2764512875f81dd9616d32f
Redchin Daniil <redchindaniil@gmail.com>
2024-09-25 21:20:07 +0700

    Added error checking to OS22X

--- src/dib_headers/os22x.cpp
+++ src/dib_headers/os22x.cpp
@@ -1,3 +1,4 @@
+#include "console_bmp/dib_headers/header_parser.hpp"
 #include "console_bmp/dib_headers/win_info.hpp"
 #include <console_bmp/bmp_reader.hpp>
 #include <console_bmp/dib_headers/os22x.hpp>
@@ -18,6 +19,8 @@ auto OS22X_Parser::is_valid_header(BmpFileType, size_t header_size) -> bool {
     return header_size == OS22X::IN_FILE_SIZE;
 }
 
+static void check_errors(OS22X header);
+
 auto OS22X_Parser::parse(std::istream& is) -> std::unique_ptr<dib_headers::HeaderBase> {
     WinInfo base_header = parse_win_info(is);
 
@@ -32,7 +35,22 @@ auto OS22X_Parser::parse(std::istream& is) -> std::unique_ptr<dib_headers::Heade
     is.ignore(4); // color encoding for each entry in the color table
     is.ignore(4); // An application-defined identifier
 
+    check_errors(header);
+
     return std::make_unique<OS22X>(header);
+}
+
+static void check_errors(OS22X header) {
+    switch (header.halftoning_alg) {
+        case HalftoningAlg::NONE:
+        case HalftoningAlg::ERROR_DIFFUSION:
+        case HalftoningAlg::PANDA:
+        case HalftoningAlg::SUPER_CIRCLE:
+            break;
+
+        default:
+            throw InvalidHeaderException("invalid halftoning algorithm specified");
+    }
 }
 
 } // namespace dib_headers
