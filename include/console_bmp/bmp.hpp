17ba52c40015fe609e20dbda8219bbc0234e7bfc
Redchin Daniil <redchindaniil@gmail.com>
2024-09-26 23:04:14 +0700

    Added an image type

--- include/console_bmp/bmp.hpp
+++ include/console_bmp/bmp.hpp
@@ -12,19 +12,19 @@ struct Rgb8Pixel {
 // Represents an abstract image with an origin at the top-left
 class Bmp {
 public:
-    virtual ~Bmp();
+    virtual ~Bmp() = 0;
 
-    virtual size_t width() const;
-    virtual size_t height() const;
+    virtual size_t width() const  = 0;
+    virtual size_t height() const  = 0;
 
-    virtual size_t num_color_channels() const;
-    virtual size_t bits_per_channel() const;
+    virtual size_t num_color_channels() const = 0;
+    virtual size_t bits_per_channel() const = 0;
 
-    virtual void get_pixel(size_t x, size_t y, void* out_buffer) const;
-    virtual void set_pixel(size_t x, size_t y, void* in_buffer) const;
+    virtual void get_pixel(size_t x, size_t y, void* out_buffer) const = 0;
+    virtual void set_pixel(size_t x, size_t y, void* in_buffer) = 0;
 
-    virtual Rgb8Pixel get_pixel_rgb8(size_t x, size_t y);
-    virtual void set_pixel_rgb8(size_t x, size_t y, Rgb8Pixel pixel);
+    virtual Rgb8Pixel get_pixel_rgb8(size_t x, size_t y) const = 0;
+    virtual void set_pixel_rgb8(size_t x, size_t y, Rgb8Pixel pixel) = 0;
 };
 
 }; // namespace console_bmp
