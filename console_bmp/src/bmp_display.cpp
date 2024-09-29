43ae42812697f795b4e96fe30cfa407452a4c5d9
Redchin Daniil <redchindaniil@gmail.com>
2024-09-29 08:06:15 +0700

    Added image opening code to CLI app

--- console_bmp/src/bmp_display.cpp
+++ console_bmp/src/bmp_display.cpp
@@ -1,5 +1,14 @@
 #include <console_bmp/bmp_display.hpp>
 
+#include <fstream>
+#include <stdexcept>
+
+#include <bmp_reader/images/rgba8.hpp>
+#include <bmp_reader/util/print.hpp>
+#include <bmp_reader/bmp_reader.hpp>
+
+using bmp_reader::println;
+
 namespace console_bmp {
 
 
@@ -8,6 +17,18 @@ BmpDisplay::BmpDisplay()
 {}
 
 void BmpDisplay::openBMP(const std::string& fileName) {
+    std::ifstream ifs(fileName, std::ios::in | std::ios::binary);
+    if (!ifs.is_open())
+        throw std::runtime_error("Failed to open the file");
+
+    
+    bmp_reader::BmpReader reader;
+    reader.add_default_parsers();
+
+    println("Gonna read that BMP file...");
+    bmp_reader::images::Rgba8 image = reader.read_bmp(ifs);
+
+    println("Image size: {} x {}", image.width(), image.height());
 
 }
 
